844f644fb3f17ec06411b6349daecd20c06feed5
d3v0ps <aitorllj93@gmail.com>
2023-09-03 11:59:43 +0200

    WIP

--- rename-as-universal.js
+++ rename-as-universal.js
@@ -5,30 +5,37 @@ const path = require("path");
 const renameSingleDirectory = (directory) => {
   const files = fs.readdirSync(directory);
 
-  files.forEach((file) => {
-    const [name, extension] = file.split(".");
+  files
+    .filter((f) => !f.startsWith("."))
+    .forEach((file) => {
+      const [name, extension] = file.split(".");
 
-    fs.mkdirSync(path.join(directory, name));
+      fs.mkdirSync(path.join(directory, name));
 
-    fs.renameSync(
-      path.join(directory, file),
-      path.join(directory, name, `Universal.${extension}`)
-    );
+      fs.renameSync(
+        path.join(directory, file),
+        path.join(directory, name, `Universal.${extension}`)
+      );
 
-    console.log(
-      `Renamed ${file} to ${path.join(
-        directory,
-        name,
-        `Universal.${extension}`
-      )}`
-    );
-  });
+      console.log(
+        `Renamed ${file} to ${path.join(
+          directory,
+          name,
+          `Universal.${extension}`
+        )}`
+      );
+    });
 };
 
-const rootFolder = "./assets2/characters/humanoids/bodyParts/facialHair/male";
+const humanoidsFolder = "./assets2/characters/humanoids";
+const clothesFolder = `${humanoidsFolder}/clothes`;
+
+const rootFolder = `${clothesFolder}/back/male/wings`;
 
 const directoriesInsideRootFolder = fs.readdirSync(rootFolder);
 
 directoriesInsideRootFolder.forEach((directory) => {
   renameSingleDirectory(path.join(rootFolder, directory));
 });
+
+// renameSingleDirectory(`${clothesFolder}/torso/male/vest/pirate_open`);
